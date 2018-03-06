2c582e74aa1567bdca966724895503bc60aa748f
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-03-06 10:38:23 +0530

    Created Registration Form.

--- app/src/main/java/com/example/sudarshan/stringzsfpolling/MainActivity.java
+++ app/src/main/java/com/example/sudarshan/stringzsfpolling/MainActivity.java
@@ -1,13 +1,106 @@
 package com.example.sudarshan.stringzsfpolling;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.input_name)
+    EditText inputName;
+    @BindView(R.id.input_email)
+    EditText inputEmail;
+    @BindView(R.id.input_password)
+    EditText inputPassword;
+    @BindView(R.id.input_college_name)
+    EditText inputCollegeName;
+    @BindView(R.id.input_course_name)
+    EditText inputCourseName;
+    @BindView(R.id.btn_signup)
+    AppCompatButton btnSignup;
+    @BindView(R.id.link_login)
+    TextView linkLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup();
+            }
+        });
+
+
     }
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        btnSignup.setEnabled(false);
+
+
+
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
+        String collegeName = inputCollegeName.getText().toString();
+        String courseName = inputCourseName.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+
+    }
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            inputName.setError("at least 3 characters");
+            valid = false;
+        } else {
+            inputName.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            inputEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            inputEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            inputPassword.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            inputPassword.setError(null);
+        }
+
+        return valid;
+    }
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Registration failed", Toast.LENGTH_LONG).show();
+
+        btnSignup.setEnabled(true);
+    }
+
 }

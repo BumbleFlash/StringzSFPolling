74ff765b4efbadb83b85d2606ad2e657223b280c
Abdulkadir98 <ak98.narutokun@gmail.com>
2018-03-06 14:48:53 +0530

    Added click listener to each item in list.

--- app/src/main/java/com/example/sudarshan/stringzsfpolling/VotingActivity.java
+++ app/src/main/java/com/example/sudarshan/stringzsfpolling/VotingActivity.java
@@ -1,10 +1,13 @@
 package com.example.sudarshan.stringzsfpolling;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
 import com.example.sudarshan.stringzsfpolling.adapters.ShortFilmAdapter;
 
@@ -26,7 +29,19 @@ public class VotingActivity extends AppCompatActivity implements ShortFilmAdapte
     }
 
     @Override
-    public void onListItemClick(int position) {
-        Toast.makeText(getApplicationContext(), "Item clicked at"+position, Toast.LENGTH_SHORT).show();
+    public void onListItemClick(int position, final ConstraintLayout layout) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                VotingActivity.this);
+        builder.setTitle("Short Film Poll!");
+        builder.setMessage("Do you want to cast your vote?");
+        builder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        layout.setBackgroundColor(Color.parseColor("#009688"));
+
+                    }
+                });
+        builder.show();
     }
 }
--- app/src/main/java/com/example/sudarshan/stringzsfpolling/adapters/ShortFilmAdapter.java
+++ app/src/main/java/com/example/sudarshan/stringzsfpolling/adapters/ShortFilmAdapter.java
@@ -2,6 +2,7 @@ package com.example.sudarshan.stringzsfpolling.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,6 +44,7 @@ public class ShortFilmAdapter extends RecyclerView.Adapter<ShortFilmAdapter.Shor
     }
     class ShortFilmViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView shortFilmTv;
+        ConstraintLayout constraintLayout;
         public ShortFilmViewHolder(View itemView) {
             super(itemView);
             shortFilmTv = itemView.findViewById(R.id.short_film_name);
@@ -52,10 +54,10 @@ public class ShortFilmAdapter extends RecyclerView.Adapter<ShortFilmAdapter.Shor
         @Override
         public void onClick(View view) {
             int position = getAdapterPosition();
-            onListItemClickListener.onListItemClick(position);
+            onListItemClickListener.onListItemClick(position, (ConstraintLayout)view);
         }
     }
     public interface OnListItemClickListener{
-        public void onListItemClick(int position);
+        public void onListItemClick(int position, ConstraintLayout layout);
     }
 }
